38cb13b071818ad2c81001641b3f8f709758c3b0
Sadhanchakma <Sadhanchakma055@gmail.com>
2026-03-28 00:20:09 +0600

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -6,13 +6,134 @@ users = load_data()
 user_steps = {}
 
 services = {
-    "Netflix": {
-        "1 Month (Share) Price 400": 400,
-        "1 Month (Private) Price 1500": 1500
+  services = {
+   "Netflix": {
+    "1 Month 1 Screen (Share) Price 350": 350,
+    "1 Month 2 Screen (Share) Price 680": 680,
+    "1 Month 3 Screen (Share) Price 1020": 1020,
+    "1 Month 4 Screen (Share) Price 1350": 1350,
+    "1 Month 5 Screen (Share) Price 1700": 1700,
+    "2 Month 1 Screen (Share) Price 680": 680,
+    "3 Month 1 Screen (Share) Price 980": 980,
+    "1 Month (Private) Price 1500": 1500
+}
+
+  "Spotify": {
+    "1 Month (Premium) Price 200": 200,
+    "3 Month (Premium) Price 580": 580,
+    "6 Month (Premium) Price 1080": 1080,
+    "12 Month (Premium) Price 2160": 2160
+}
+
+    "YouTube Premium": {
+        "1 Month (Premium) Price 120": 120,
+        "1 Year (Premium) Price 2899": 2899
     },
-    "Spotify": {
-        "1 Month (Share) Price 230": 230,
-        "1 Month (Private)Price 700": 700
+
+    "Amazon Prime": {
+        "1 Month (Premium) Price 100": 100,
+        "3 Month (Premium) Price 300": 300,
+        "6 Month (Premium) Price 600": 600,
+        "12 Month (Premium) Price 1200": 1200
+    },
+
+    "Crunchyroll": {
+        "1 Month (Premium) Price 180": 180,
+        "3 Month (Premium) Price 499": 499,
+        "6 Month (Premium) Price 1080": 1080,
+        "12 Month (Premium) Price 2160": 2160
+    },
+
+    "Hulu": {
+        "1 Month (Premium) Price 230": 230,
+        "3 Month (Premium) Price 650": 650,
+        "6 Month (Premium) Price 1200": 1200,
+        "12 Month (Premium) Price 2400": 2400
+    },
+
+    "Sony LIV": {
+        "1 Month (Premium) Price 220": 220,
+        "3 Month (Premium) Price 650": 650,
+        "6 Month (Premium) Price 1080": 1080,
+        "12 Month (Premium) Price 2160": 2160
+    },
+
+    "Disney+": {
+        "1 Month (Premium) Price 350": 350,
+        "3 Month (Premium) Price 1050": 1050
+    },
+
+    "Disney+ Hotstar": {
+        "1 Month (Premium) Price 180": 180,
+        "3 Month (Premium) Price 540": 540,
+        "6 Month (Premium) Price 1080": 1080,
+        "12 Month (Premium) Price 2160": 2160
+    },
+
+    "HBO Max": {
+        "1 Month (Premium) Price 180": 180,
+        "3 Month (Premium) Price 540": 540,
+        "6 Month (Premium) Price 1080": 1080,
+        "12 Month (Premium) Price 2160": 2160
+    },
+
+    "Chorki": {
+        "1 Month (Premium) Price 150": 150,
+        "3 Month (Premium) Price 450": 450,
+        "6 Month (Premium) Price 1080": 1080,
+        "12 Month (Premium) Price 2160": 2160
+    },
+
+    "Telegram Premium": {
+        "1 Month (Premium) Price 600": 600,
+        "3 Month (Premium) Price 1800": 1800,
+        "6 Month (Premium) Price 3600": 3600,
+        "12 Month (Premium) Price 7200": 7200
+    },
+
+    "Canva Pro": {
+        "1 Month (Premium) Price 30": 30,
+        "3 Month (Premium) Price 90": 90,
+        "6 Month (Premium) Price 180": 180,
+        "12 Month (Premium) Price 360": 360,
+        "Lifetime (Premium) Price 499": 499
+    },
+
+    "ChatGPT": {
+        "1 Month (Premium) Price 600": 600,
+        "3 Month (Premium) Price 1800": 1800,
+        "6 Month (Premium) Price 3600": 3600,
+        "12 Month (Premium) Price 7200": 7200
+    },
+
+    "PUBG UC": {
+        "120 UC Price 210": 210,
+        "180 UC Price 300": 300,
+        "376 UC Price 1050": 1050,
+        "660 UC Price 1990": 1990,
+        "780 UC Price 2300": 2300,
+        "1800 UC Price 5200": 5200,
+        "2175 UC Price 6000": 6000,
+        "4000 UC Price 10500": 10500,
+        "8400 UC Price 19900": 19900,
+        "16800 UC Price 39000": 39000,
+        "33600 UC Price 78000": 78000,
+        "42000 UC Price 99000": 99000
+    },
+
+    "ML Diamonds": {
+        "14 Diamond Price 60": 60,
+        "42 Diamond Price 180": 180,
+        "70 Diamond Price 300": 300,
+        "140 Diamond Price 600": 600,
+        "284 Diamond Price 1200": 1200,
+        "355 Diamond Price 1500": 1500,
+        "429 Diamond Price 1800": 1800,
+        "716 Diamond Price 3000": 3000,
+        "1446 Diamond Price 6000": 6000,
+        "2976 Diamond Price 12000": 12000,
+        "7502 Diamond Price 20440": 20440,
+        "10478 Diamond Price 28000": 28000
     }
 }
 
